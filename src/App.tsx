71a061ff600fb9dbfd333e9cb754493d68e224ac
zizifn <1803942+zizifn@users.noreply.github.com>
2025-03-21 21:40:13 +0800

    book title

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+async function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        title: "The Hobbit",
+        authors: ["J.R.R. Tolkien"],
+        thumbnail: "https://ui.dev/images/courses/query/hobbit.jpg",
+      });
+    }, 1000);
+  });
 }
 
-export default App
+function usBook() {
+  return useQuery({
+    queryKey: ["test"],
+    queryFn: getData,
+  });
+}
+
+function Book() {
+  const { data, isPending, error, isSuccess, status } = usBook();
+  return (
+    <div>
+      <header className="app-header">
+        <h1>
+          <span>Query Library</span>
+        </h1>
+      </header>
+      {isPending && <Loading></Loading>}
+      {error && <Error></Error>}
+      {isSuccess && (
+        <main>
+          <h2 className="book-title">{data.title}</h2>
+          <p>By: {data.authors?.join(", ")}</p>
+          {data.thumbnail && <img src={data.thumbnail} alt={data.title} />}
+        </main>
+      )}
+    </div>
+  );
+}
+function Loading() {
+  return <main>Loading...</main>;
+}
+
+function Error() {
+  return <main>Woops there was an error...</main>;
+}
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Book />;
+    </QueryClientProvider>
+  );
+}
